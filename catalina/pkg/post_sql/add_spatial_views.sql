ea65d99f8467818ec5386f81eb53fac843e4614b
Ellery Ames <ellery.ames@flaxandteal.co.uk>
2026-08-07 05:31:30 -0400

    fix: remove unnecessary spatial views
    areas and assessments do not contain geometry in the arches db, so no spatial view will show up.

--- catalina/pkg/post_sql/add_spatial_views.sql
+++ catalina/pkg/post_sql/add_spatial_views.sql
@@ -20,12 +20,6 @@
 DROP VIEW IF EXISTS public.monument_point CASCADE;
 DROP VIEW IF EXISTS public.monument_linestring CASCADE;
 DROP VIEW IF EXISTS public.monument_polygon CASCADE;
-DROP VIEW IF EXISTS public.area_point CASCADE;
-DROP VIEW IF EXISTS public.area_linestring CASCADE;
-DROP VIEW IF EXISTS public.area_polygon CASCADE;
-DROP VIEW IF EXISTS public.consultation_point CASCADE;
-DROP VIEW IF EXISTS public.consultation_linestring CASCADE;
-DROP VIEW IF EXISTS public.consultation_polygon CASCADE;
 
 -- Heritage places (monument)
 DELETE FROM public.spatial_views WHERE spatialviewid = '27318c10-adc4-421c-9e93-9c007ceee035';
@@ -62,66 +56,5 @@ VALUES (
     ]',
     true,
     '87d3d7dc-f44f-11eb-bee9-a87eeabdefba'::UUID,
-    'en'
-);
-
--- Areas
-DELETE FROM public.spatial_views WHERE spatialviewid = 'bdbd2722-70dd-4c2b-ad56-a0622c6acff7';
-
-INSERT INTO public.spatial_views (
-    spatialviewid,
-    schema,
-    slug,
-    description,
-    ismixedgeometrytypes,
-    attributenodes,
-    isactive,
-    geometrynodeid,
-    languageid
-)
-VALUES (
-    'bdbd2722-70dd-4c2b-ad56-a0622c6acff7',
-    'public',
-    'area',
-    'Areas',
-    false,
-    '[
-        {"nodeid":"f45dbbe8-80b7-11ea-b325-f875a44e0e11","description":"area_name_n1"},
-        {"nodeid":"8dca12bd-edeb-11eb-a6c6-a87eeabdefba","description":"legacy_id"}
-    ]',
-    true,
-    '64be56e3-3ee5-11eb-b1f0-f875a44e0e11'::UUID,
-    'en'
-);
-
--- Consultations
-DELETE FROM public.spatial_views WHERE spatialviewid = 'a9053144-4127-4519-b0e4-d5572c7c6b24';
-
-INSERT INTO public.spatial_views (
-    spatialviewid,
-    schema,
-    slug,
-    description,
-    ismixedgeometrytypes,
-    attributenodes,
-    isactive,
-    geometrynodeid,
-    languageid
-)
-VALUES (
-    'a9053144-4127-4519-b0e4-d5572c7c6b24',
-    'public',
-    'consultation',
-    'Consultations',
-    false,
-    '[
-        {"nodeid":"4ad69684-951f-11ea-b5c3-f875a44e0e11","description":"consultation_name"},
-        {"nodeid":"40eff4cd-893a-11ea-b0cc-f875a44e0e11","description":"log_date"},
-        {"nodeid":"82f8a166-951a-11ea-bdad-f875a44e0e11","description":"consultation_description"},
-        {"nodeid":"8d41e4e3-a250-11e9-89d8-00224800b26d","description":"planning_outcome"},
-        {"nodeid":"3b500558-eec2-11eb-98b3-a87eeabdefba","description":"external_cross_reference"}
-    ]',
-    true,
-    'b949053a-184f-11eb-ac4a-f875a44e0e11'::UUID,
     'en'
 );
