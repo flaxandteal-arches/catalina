92cf8bbc496492a6b9194768cbc4a7009943f86f
Andrea K. <andrea.kyurchiev@flaxandteal.co.uk>
2026-09-25 13:33:31 +0300

    fix: fork arches_her to fix missing arches_controlled_lists migration

--- catalina/settings.py
+++ catalina/settings.py
@@ -198,7 +198,7 @@ INSTALLED_APPS = (
     "django_hosts",
     "arches_controlled_lists",
     "arches_querysets",
-    "arches_component_lab",
+    "arches_vue_components",
     "arches",
     "arches.app.models",
     "arches.management",
